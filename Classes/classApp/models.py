e958b61c022e84c87516ee40aac29ef0b3a665db
brandonlyon24 <72480983+brandonlyon24@users.noreply.github.com>
2020-12-20 12:32:45 -0800

    django upload

--- Classes/classApp/models.py
+++ Classes/classApp/models.py
@@ -1,16 +1,13 @@
 from django.db import models
 
-
+#model class, register in admin.py
 class djangoClasses(models.Model):
     title = models.CharField(max_length=100, default="", blank=True, null=False)
-    courseNumber = models.IntegerField(max_length=50, default="", blank=True, null=False)
+    courseNumber = models.IntegerField(default="", blank=True, null=False)
     instructorName = models.CharField(max_length=30, default="", blank=True, null=False)
-    duration = models.FloatField(max_length=100, default="", blank=True, null=False)
+    duration = models.FloatField(default="", blank=True, null=False)
 
     objects = models.Manager()
 
     def __str__(self):
         return self.title
-
-
-
